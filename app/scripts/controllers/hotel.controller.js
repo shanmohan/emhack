5bd34b6355e572e244766faeb8336c2946da64eb
clovis.gakam <clovis.gakam@gmail.com>
2015-08-28 23:54:58 +0400

    update car service

--- app/scripts/controllers/hotel.controller.js
+++ app/scripts/controllers/hotel.controller.js
@@ -3,11 +3,20 @@
  */
 
 angular.module('emhackApp').controller('HotelController', function ($scope,skyscannerService) {
-	
-	$scope.getCarOptions = function getCarOptions(){
-	var carOption = 	skyscannerService.getCarOptions() ;
-	}
 
-	
+  $scope.cars = [];
+	$scope.getCarOptions = function getCarOptions(){
+	skyscannerService.getCarOptions().then(function(response){
+    $scope.cars = [];
+    for(var i=0;i<response.data.cars.length;i++){
+      var elt =response.data.cars[i];
+      elt.url = response.data.images[i].url;
+      if(i===100)break;
+      $scope.cars.push(elt);
+    }
+    console.log($scope.cars);
+  }) ;
+
+	}
 
 })
--- app/scripts/services/skysacanner.service.js
+++ app/scripts/services/skysacanner.service.js
@@ -8,18 +8,14 @@ angular.module('emhackApp').service('skyscannerService', skyscannerService);
 
 skyscannerService.$inject = ['$http','$log'];
 
-function skyscannerService($http,$log){
+function skyscannerService($http,$log) {
   return {
-    getCarOptions : getCarOptions
+    getCarOptions: getCarOptions
   };
 
-  function getCarOptions(){
-    var carOptionQuery = "http://business.skyscanner.net/apiservices/carhire/liveprices/v2/UK/GBP/en-GB/27544008/27544008/2015-09-04T10%3A00/2015-09-11T10%3A00/21";
-    return $http.get(carOptionQuery,{userip:'127.0.0.1',apikey:'prtl6749387986743898559646983194'}).then(function(response){
-      $log.info(response.data);
-      return response.data
-    },function(error){
-    	$log.info(error);
-    })
+  function getCarOptions() {
+    var carOptionQuery = "http://business.skyscanner.net/apiservices/carhire/liveprices/v2/UK/GBP/en-GB/27544008/27544008/2015-09-04T10%3A00/2015-09-11T10%3A00/21?userip=127.0.0.1&apikey=prtl6749387986743898559646983194";
+    var hotelQuery = "http://business.skyscanner.net/apiservices/hotels/liveprices/v2/H4sIAAAAAAAEAE2IMQqAMBAE_7L1Be4uCcZ0EYMfsBNLuxArK_HvRqvAMjvMjRNxQ8ogHNUsU_uU50YdvHPM4VMWb3g07Fbm-K-rIl1VEoJY0iZeQPUqZSdURCNqJTwvSLzd7HEAAAA1?apikey=_Tx5reSAm4wQDLdI0YUpeUQx-fWPoDeJo1sH-kkKMdlbRRkJGQkD4278LRSz2wfh-pXySNIFyGfBP8C1zaoSMSg%3D%3D"
+    return  $http.get(carOptionQuery);
   }
 }
