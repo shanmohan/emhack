9df7ee0169abc53d26556950256570ae5a063478
clovis.gakam <clovis.gakam@gmail.com>
2015-08-29 02:34:35 +0400

    umpdate

--- app/scripts/controllers/hotel.controller.js
+++ app/scripts/controllers/hotel.controller.js
@@ -5,14 +5,21 @@
 angular.module('emhackApp').controller('HotelController', function ($scope,skyscannerService) {
 
   $scope.cars = [];
+  $scope.carImages = ["http://logos.skyscnr.com/images/carhire/sippmaps/volkswagen_beetle.jpg",
+    "http://logos.skyscnr.com/images/carhire/sippmaps/toyota_auris_hybrid.jpg",
+    "http://logos.skyscnr.com/images/carhire/sippmaps/peugeot_308.jpg",
+    "http://logos.skyscnr.com/images/carhire/sippmaps/mazda_cx-5.jpg",
+    "http://logos.skyscnr.com/images/carhire/sippmaps/citroen_berlingo.jpg"];
 	$scope.getCarOptions = function getCarOptions(){
 	skyscannerService.getCarOptions().then(function(response){
-    $scope.cars = [];
+    $scope.cars = []
     for(var i=0;i<response.data.cars.length;i++){
-      var elt =response.data.cars[i];
-      elt.url = response.data.images[i+10].url;
       if(i===5)break;
+      var elt =response.data.cars[i];
+      elt.url = $scope.carImages[i];
       $scope.cars.push(elt);
+
+
     }
     console.log($scope.cars);
   }) ;
