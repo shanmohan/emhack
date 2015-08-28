48d0ebc229a0888cf77efd5b08d4ec8e8233c1b1
shanmohan <shanmohan@gmail.com>
2015-08-28 10:20:41 +0400

    initila upload

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngDirDepthNWideApp
+ * @description
+ * # ngDirDepthNWideApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngDirDepthNWideApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngDirDepthNWideApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngDirDepthNWideApp
+ */
+angular.module('ngDirDepthNWideApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
