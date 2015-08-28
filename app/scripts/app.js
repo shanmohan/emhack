b5f061b7c871fdd2498c390c9e17558a3fa5b3d2
shanmohan <shanmohan@gmail.com>
2015-08-28 17:58:46 +0400

    streetview implemented

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,22 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name ngDirDepthNWideApp
- * @description
- * # ngDirDepthNWideApp
- *
- * Main module of the application.
- */
 angular
-  .module('emhackApp', ['rzModule']);
+  .module('emhackApp', ['rzModule','ui.router']).config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('main', {
+      url: '/',
+      templateUrl: '/views/main.html',
+      controller: 'MainController'
+    }).state('streetview', {
+      url: '/streetview',
+      templateUrl : '/views/street-view.html',
+      controller: 'StreetController'
+    }).state('hotels', {
+      url: '/hotels',
+      templateUrl : '/views/hotels.html',
+      controller: 'HotelController'
+    });
+
+  })
